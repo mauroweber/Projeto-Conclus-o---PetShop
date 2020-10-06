3064bbfd2dc56f38b88dcd328055d61582e6357c
Mauro Weber <mauro.weber@live.com>
2020-10-05 22:31:08 -0300

    gitflow-feature-stash: Mauro123

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,45 +1,59 @@
-import React, { useState } from "react";
-import { PageArea } from "./styled";
+const { default: Api } = require("../../helpers/Api");
+import React, { Component } from 'react';
+import { PageArea } from "../SignIn/styled";
+import { PageContainer} from "../../components/MainComponets";
 
-import { PageContainer } from "../../components/MainComponets";
-
-const Page = () => {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  };
-
-  return (
+class SignIn extends Component {
+    render() {
     <PageContainer>
       <PageArea>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <form onSubmit={handleSubmit}>
-          <label className="area">
-            <div className="area--title">Nome Completo:</div>
-            <div className="area--input">
-              <input type="text" />
-            </div>
-          </label>
           <label className="area">
             <div className="area--title">E-mail:</div>
             <div className="area--input">
-              <input type="email" />
+              <input
+                type="email"
+                disabled={disabled}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
           </label>
           <label className="area">
             <div className="area--title">Senha:</div>
             <div className="area--input">
-              <input type="password" />
+              <input
+                type="password"
+                disabled={disabled}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Lembrar Senha:</div>
+            <div className="area--input">
+              <input
+                type="checkbox"
+                disabled={disabled}
+                checked={remenberPassword}
+                onChange={() => setRemenberPassword(!remenberPassword)}
+              />
             </div>
           </label>
           <label className="area">
             <div className="area--title"></div>
             <div className="area--input">
-              <button>Fazer Cadastro</button>
+              <button disabled={disabled}>Fazer Login</button>
             </div>
           </label>
         </form>
       </PageArea>
     </PageContainer>
-  );
-};
+    }
+}
 
-export default Page;
+export default SignIn;
